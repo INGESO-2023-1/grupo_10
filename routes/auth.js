51b8e18161509e6a8c3d55e00fd824c96530fd73
moxwel <49621788+moxwel@users.noreply.github.com>
2023-05-16 00:05:55 -0400

    auth post console text

--- routes/auth.js
+++ routes/auth.js
@@ -4,14 +4,16 @@ var router = express.Router();
 // Manejar peticiones POST
 
 router.post('/login', function(req, res, next) {
-  // Show the req.body in the webpage as string
-  res.send("POST Login recibido. Peticion: " + JSON.stringify(req.body));
+  // Show the req.body in the console then go back to "/"
+  console.log("POST Login recibido. Peticion: " + JSON.stringify(req.body));
+  res.redirect('/');
 
 });
 
 router.post('/register', function(req, res, next) {
-  // Show the req.body in the webpage as string
-  res.send("POST Register recibido. Peticion: " + JSON.stringify(req.body));
+  // Show the req.body in the console then go back to "/"
+  console.log("POST Register recibido. Peticion: " + JSON.stringify(req.body));
+  res.redirect('/');
 });
 
 module.exports = router;
