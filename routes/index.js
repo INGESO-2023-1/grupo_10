240f09ec2df1013e8cdbbe154a763a2d7d0df4be
moxwel <49621788+moxwel@users.noreply.github.com>
2023-05-15 20:20:09 -0400

    Static files

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Wasap' });
+  res.sendFile('pages/index.html', { root: 'public' });
+});
+
+router.get('/home', function(req, res, next) {
+  res.sendFile('pages/home.html', { root: 'public' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.sendFile('pages/login.html', { root: 'public' });
+});
+
+router.get('/register', function(req, res, next) {
+  res.sendFile('pages/register.html', { root: 'public' });
 });
 
 module.exports = router;
