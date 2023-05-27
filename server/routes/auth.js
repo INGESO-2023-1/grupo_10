dbb0b64cc089ba5d60730ba26f94103ae696e4db
moxwel <49621788+moxwel@users.noreply.github.com>
2023-05-27 14:54:07 -0400

    add post

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,9 +4,14 @@ const router = express.Router();
 // Archivos de controladores
 const authController = require('../controllers/auth');
 
-// Manejar peticiones GET
+// Manejar peticiones GET (usando req.query)
 router.get('/', authController.prueba);
 router.get('/login', authController.login);
 router.get('/register', authController.register);
+
+// Manejar peticiones POST (usando req.body)
+router.post('/', authController.prueba);
+router.post('/login', authController.login);
+router.post('/register', authController.register);
 
 module.exports = router;
