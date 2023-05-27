bf235b54fcc526aba1b8ab2ce8a96ea1e90c4b30
moxwel <49621788+moxwel@users.noreply.github.com>
2023-05-27 14:54:28 -0400

    fix request

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,14 +1,23 @@
-module.exports.prueba = function (req, res, next) {
-  // Show the req.body in the page then go back to "/"
-  res.send("Prueba recibido. Peticion: " + JSON.stringify(req.body));
+module.exports.prueba = function (req, res) {
+  if (req.method == "GET") {
+    res.send("Prueba recibida. Peticion GET: " + JSON.stringify(req.query));
+  } else if (req.method == "POST") {
+    res.send("Prueba recibida. Peticion POST: " + JSON.stringify(req.body));
+  }
 }
 
-module.exports.login = function (req, res, next) {
-  // Show the req.body in the page then go back to "/"
-  res.send("Login recibido. Peticion: " + JSON.stringify(req.body));
+module.exports.login = function (req, res) {
+  if (req.method == "GET") {
+    res.send("Login recibido. Peticion GET: " + JSON.stringify(req.query));
+  } else if (req.method == "POST") {
+    res.send("Login recibido. Peticion POST: " + JSON.stringify(req.body));
+  }
 }
 
-module.exports.register = function (req, res, next) {
-  // Show the req.body in the page then go back to "/"
-  res.send("Register recibido. Peticion: " + JSON.stringify(req.body));
+module.exports.register = function (req, res) {
+  if (req.method == "GET") {
+    res.send("Register recibido. Peticion GET: " + JSON.stringify(req.query));
+  } else if (req.method == "POST") {
+    res.send("Register recibido. Peticion POST: " + JSON.stringify(req.body));
+  }
 }
