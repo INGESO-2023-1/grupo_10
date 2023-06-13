4cc25550e89f7ec374537cdf199c573d29daaf2c
Max Sepúlveda <49621788+moxwel@users.noreply.github.com>
2023-06-12 20:47:16 -0400

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Navbar from "./components/navbar";
+import Contactos from "./components/contactos";
+import Chat from "./components/chat";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>      
+      <Navbar />
+      <br/>
+      <Routes>
+        <Route path="/contactos" element={<Contactos />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ client/src/components/contactos.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useEffect, useState } from "react";
+//import Axios from "axios";
+
+function Contactos() {
+    //const [var1, var2] = useState([]);
+    /*
+    useEffect(()=> {
+      Axios.get("url").then((response)=>{var2(response.data)})
+    }, [] );
+    */
+  
+    return (
+        <table class="default">
+            <tr>
+                <td>Ignacio</td>
+            </tr>
+            <tr>
+                <td>Sebastian</td>
+            </tr>
+        
+        </table>
+    );
+}
+export default Contactos;
