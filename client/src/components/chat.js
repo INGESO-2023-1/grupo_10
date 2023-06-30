53248ffed64e063d26159600ce940f5d8534ee4e
seed4407 <124889238+seed4407@users.noreply.github.com>
2023-06-29 22:46:56 -0400

    mostrar chat
    muestra el chat, pero no como debe

--- client/src/components/chat.js
+++ client/src/components/chat.js
@@ -6,7 +6,9 @@ import Nav from 'react-bootstrap/Nav';
 import Row from 'react-bootstrap/Row';
 import Tab from 'react-bootstrap/Tab';
 import Card from 'react-bootstrap/Card';
+import axios from 'axios';
 
+axios.defaults.withCredentials = true;
 
 function Chat() {
   return (
@@ -53,6 +55,36 @@ function Chat() {
             <Tab.Pane eventKey="primero">
               <Card style={{ width: '60rem', height: '430px', backgroundColor: 'rgba(0, 191, 255, 0.2)' }}>
                 <Card.Body>
+                  <Card.Title id='numero'>5555555555</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
+                  <Card.Text>
+                  </Card.Text>
+                  <div class="container">
+                    <div class="row">
+                      <div class="col">
+                        <Card style={{ width: '55rem', height: '300px' }}>
+                          <Card.Body>
+                            <Card.Title></Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
+                            <Card.Text>
+                              <p id='chat1'></p>
+                            </Card.Text>
+                          </Card.Body>
+                        </Card>
+                      </div>
+                      <div class="w-100"></div>
+                      <div class="col">
+                        <input type="text" placeholder="Escribe aquí" id='mensaje' style={{ width: '55rem',borderRadius: '8px',marginTop: '7px' }}/>
+                        <button style={{borderRadius: '10px', marginTop: '4px'}} onClick={eniar_mensaje}>Enviar</button>
+                      </div>
+                    </div>
+                  </div>
+                </Card.Body>
+              </Card>
+            </Tab.Pane>
+            <Tab.Pane eventKey="segundo">
+            <Card style={{ width: '60rem', height: '430px', backgroundColor: 'rgba(0, 191, 255, 0.2)' }}>
+                <Card.Body>
                   <Card.Title>Numero receptor</Card.Title>
                   <Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
                   <Card.Text>
@@ -71,14 +103,14 @@ function Chat() {
                       </div>
                       <div class="w-100"></div>
                       <div class="col">
-                        <input type="text" placeholder="Escribe aquí" style={{ width: '55rem',borderRadius: '8px',marginTop: '7px' }}/>
+                        <input type="text" placeholder="Escribe aquí" id= 'mensaje' style={{ width: '55rem',borderRadius: '8px',marginTop: '7px' }}/>
                       </div>
                     </div>
                   </div>
                 </Card.Body>
               </Card>
+            
             </Tab.Pane>
-            <Tab.Pane eventKey="segundo">Second tab content</Tab.Pane>
           </Tab.Content>
         </Col>
       </Row>
@@ -93,3 +125,54 @@ function Chat() {
 
 
 export default Chat;
+
+function eniar_mensaje() {
+  if(document.getElementById("numero") !== null){
+  axios.post('http://localhost:4000/api/message/send', {phone:  document.getElementById("numero").textContent, message: document.getElementById("mensaje").value})
+  .then(response => {
+    // var mostrar = document.getElementById("incorrecto");
+    // mostrar.style.visibility = "hidden";
+    // mostrar = document.getElementById("correcto");
+    // mostrar.style.visibility = "visible"
+    console.log(response.data);
+    mensajeria();
+  })
+  .catch(error => {
+    if(error.name === "AxiosError"){
+      // var mostrar = document.getElementById("incorrecto");
+      // mostrar.style.visibility = "visible";
+      // mostrar = document.getElementById("correcto");
+      // mostrar.style.visibility = "hidden"
+    }
+    console.error(error);
+  });
+}
+}
+
+function mensajeria(){
+  let variable = "";
+  if(document.getElementById("numero") !== null){
+  let texto ='http://localhost:4000/api/message/list/'+document.getElementById("numero").textContent;
+  axios.get(texto)
+  .then(response => {
+    var largo_lista = response.data.length;
+    var contador = 0;
+    var dato1;
+    var dato2;
+    console.log(dato1); 
+    while(contador<largo_lista){
+      dato1 = response.data[contador].fromPhone.toString();
+      dato2 = response.data[contador].message.toString();
+      variable = <div>{variable}<p>{dato1}: {dato2}</p></div>;
+      contador = contador +1;
+    }
+    var chat = document.getElementById("chat1");
+    chat.innerHTML = variable;
+    console.log("%c" + variable, "color: blue;");
+  })
+  .catch(error => {
+    console.error(error);
+
+  }); 
+  }
+}
